8e33df786a3997451e65d4eafb53c26be3cd0e21
AlanGL98 <alangl.lmad@hotmail.com>
2020-09-15 15:17:36 -0500

    Agregamos estilos al menu lateral
    Se agrego estilos al menu lateral, ahora es desplegable y tambien un sub menu desplegable dentro de este.

---
+++ js/main.js
@@ -0,0 +1,23 @@
+//Botones para desplebar el menu lateral del ucuario
+var navMenu = document.querySelector('#nav-menu');
+var btnExitMenu = document.querySelector('#btn-exit-menu');
+
+//Boton para desplegar el sub menu del usuario y sus opciones
+var btnSubMenu = document.querySelector('#btn-sub-menu-user');
+
+//MENU-------------------------------------------------------------------//
+navMenu.addEventListener('click', ()=>{
+    var menu = document.querySelector('#menu');
+    menu.classList.toggle('menu-activo');
+});
+
+btnExitMenu.addEventListener('click', ()=>{
+    var menu = document.querySelector('#menu');
+    menu.classList.toggle('menu-activo');
+});
+
+//SUB MENU--------------------------------------------------------------//
+btnSubMenu.addEventListener('click', ()=>{
+    var subMenu = document.querySelector('#sub-menu-user');
+    subMenu.classList.toggle('sub-activo');
+});
