a4d6c7186882480d40cbf86192685d116d3b7fff
AlanGL98 <alangl.lmad@hotmail.com>
2020-09-24 15:21:22 -0500

    Agregamos estilos al footer
    Se agregaron estilos al footer asi como sus respectivos botones

--- js/main.js
+++ js/main.js
@@ -1,28 +1,25 @@
-//Botones para desplebar el menu lateral del ucuario
-var navMenu = document.querySelector('#nav-menu');
-var btnExitMenu = document.querySelector('#btn-exit-menu');
+window.onload = () => {
+    //Botones para desplebar el menu lateral del ucuario
+    let navMenu = document.querySelector('#nav-menu');
+    let btnExitMenu = document.querySelector('#btn-exit-menu');
 
-//Boton para desplegar el sub menu del usuario y sus opciones
-var btnSubMenu = document.querySelector('#btn-sub-menu-user');
+    //Boton para desplegar el sub menu del usuario y sus opciones
+    let btnSubMenu = document.querySelector('#btn-sub-menu-user');
 
-//MENU-------------------------------------------------------------------//
-navMenu.addEventListener('click', ()=>{
-    var menu = document.querySelector('#menu');
-    menu.classList.toggle('menu-activo');
-});
+    //MENU-------------------------------------------------------------------//
+    navMenu.addEventListener('click', ()=>{
+        let menu = document.querySelector('#menu');
+        menu.classList.toggle('menu-activo');
+    });
 
-btnExitMenu.addEventListener('click', ()=>{
-    var menu = document.querySelector('#menu');
-    menu.classList.toggle('menu-activo');
-});
+    btnExitMenu.addEventListener('click', ()=>{
+        let menu = document.querySelector('#menu');
+        menu.classList.toggle('menu-activo');
+    });
 
-//SUB MENU--------------------------------------------------------------//
-btnSubMenu.addEventListener('click', ()=>{
-    var subMenu = document.querySelector('#sub-menu-user');
-    subMenu.classList.toggle('sub-activo');
-});
-
-
-// efecto.addEventListener('mouseover', ()=>{
-//     alert('hola hola hola');
-// });
+    //SUB MENU--------------------------------------------------------------//
+    btnSubMenu.addEventListener('click', ()=>{
+        let subMenu = document.querySelector('#sub-menu-user');
+        subMenu.classList.toggle('sub-activo');
+    });
+};
