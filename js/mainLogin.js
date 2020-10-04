160a51d70f77d7b70a982449eb133852d9c64f8b
AlanGL98 <alangl.lmad@hotmail.com>
2020-10-04 16:37:29 -0500

    Agregamos Login y Crear cuenta
    Se agrego login y creacion de cuenta, tanto html, css y js

---
+++ js/mainLogin.js
@@ -0,0 +1,40 @@
+window.onload = () => {
+    let login = document.querySelector('#login');
+    let createAccount = document.querySelector('#create-account');
+
+    login.addEventListener('click', ()=>{
+        let panelLogin = document.querySelector('#panel-login');
+        let panelCrear = document.querySelector('#panel-create-account');
+        panelLogin.classList.add('activar');
+        panelCrear.classList.remove('activar');
+    });
+
+    createAccount.addEventListener('click', ()=>{
+        let panelLogin = document.querySelector('#panel-login');
+        let panelCrear = document.querySelector('#panel-create-account');
+        panelLogin.classList.remove('activar');
+        panelCrear.classList.add('activar');
+    });
+
+    //Validaciones Login
+    let formLogin = document.querySelector('#form-login'),
+        error = document.querySelector('#error');
+
+    function validaCorreo(e){
+		if (formLogin.correo.value == '' || formLogin.correo.value == null){
+			error.style.width = '80%';
+			error.innerHTML += '<li>Correo incorrecto.</li>';
+
+			e.preventDefault();
+		} 
+    }
+
+    function validaFormulario(e){
+        // Reiniciamos el error para que inicie sin mensaje.
+        error.innerHTML = '';
+        
+        validaCorreo(e);
+    }
+
+    formLogin.addEventListener('click', validaFormulario);
+}
